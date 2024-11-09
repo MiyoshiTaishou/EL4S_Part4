3357412a665cfc12d699f97c4f4ab33da66ca947
Xenophie <y.kuya1217@gmail.com>
2024-11-09 15:54:45 +0900

    タイトルシーン追加（シーン遷移はテストしてない）

---
+++ EL4S_Part4/Assets/Script/Yasaka/TitleScript.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TitleScript : MonoBehaviour
+{
+    [SerializeField]
+    string sceneName;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Return) || Input.GetButtonDown("Fire1")) ;
+        {
+            StartCoroutine(SceneLoadAsync());
+        }
+    }
+
+    IEnumerator SceneLoadAsync()
+    {
+        var async = SceneManager.LoadSceneAsync(sceneName);
+
+        // ÉçÅ[ÉhÇ™äÆóπÇ∑ÇÈÇ‹Ç≈ë“ã@Ç∑ÇÈ
+        while (!async.isDone)
+        {
+            yield return null;
+        }
+    }
+}
