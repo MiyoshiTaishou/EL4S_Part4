eac089fe28cc367007f28b35a51d3aeee6028cc7
Xenophie <y.kuya1217@gmail.com>
2025-01-11 10:07:37 +0900

    Add : リザルトシーン

---
+++ EL4S_Part4/Assets/Script/Yasaka/ResultScript.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ResultScript : MonoBehaviour
+{
+    [SerializeField]
+    TextMeshProUGUI textMesh;
+
+    [SerializeField]
+    string sceneName;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //SetScoreText( ここにスコア変数を );
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Return) || Input.GetButtonDown("Fire1"))
+        {
+            PressAnyKey();
+        }
+    }
+
+    public void SetScoreText(float _score)
+    {
+        textMesh.text = _score.ToString();
+    }
+
+    void PressAnyKey()
+    {
+        StartCoroutine(SceneLoadAsync());
+    }
+
+    IEnumerator SceneLoadAsync()
+    {
+        var async = SceneManager.LoadSceneAsync(sceneName);
+
+        // ロードが完了するまで待機する
+        while (!async.isDone)
+        {
+            yield return null;
+        }
+    }
+}
