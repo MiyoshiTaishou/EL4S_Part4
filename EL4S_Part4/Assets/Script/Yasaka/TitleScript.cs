0e2f45c796def3b9b0e3e7737b0d50be31dee908
MiyoshiTaishou <99778743+MiyoshiTaishou@users.noreply.github.com>
2025-01-11 09:56:09 +0900

    タイトルからゲーム

--- EL4S_Part4/Assets/Script/Yasaka/TitleScript.cs
+++ EL4S_Part4/Assets/Script/Yasaka/TitleScript.cs
@@ -18,7 +18,7 @@ public class TitleScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Return) || Input.GetButtonDown("Fire1")) ;
+        if (Input.GetKeyDown(KeyCode.Return) || Input.GetButtonDown("Fire1"))
         {
             StartCoroutine(SceneLoadAsync());
         }
